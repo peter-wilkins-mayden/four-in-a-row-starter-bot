f4ec498cc63fc9ca222b8ef299d79fc53a1f6b53
Caleb Phillips <calebrphillips@gmail.com>
2016-03-12 17:04:41 -0500

    Remove dynamic naming convention

--- bot.clj
+++ bot.clj
@@ -1,25 +1,29 @@
 (ns bot
   (:require [clojure.string :as str]))
 
+(defn check-winner [board] ())
+
 ;Define the mutable program state
-(def *settings* (atom {}))
-(def *updates* (atom {}))
-(def *board* (atom []))
+(def settings (atom {}))
+(def updates (atom {}))
+(def board (atom []))
 
 ;Update the board with parsed data (list of vectors)
+;Note that it stores the given rows in reverse order from what the server
+;provides... it just seemed more sensible
 (defn update-board [board-string] 
   (let [vs (vec (str/split board-string #";"))] 
-    (reset! *board* (map #(str/split % #",") vs))))
+    (reset! board (reverse (map #(str/split % #",") vs)))))
 
 ;Save settings
 (defn save-setting [[k v]] 
-  (swap! *settings* assoc k (read-string v)))
+  (swap! settings assoc k (read-string v)))
 
 ;Save game updates
 (defn game-update [[about k v]] 
   (if (= k "field")
     (update-board v)
-    (swap! *updates* assoc-in [about k] (read-string v))))
+    (swap! updates assoc-in [about k] (read-string v))))
 
 ;returns a random action
 (defn get-action [[action t]] 
@@ -32,7 +36,7 @@
       "settings" (save-setting args)
       "update" (game-update args)
       "action" (println (get-action args))
-      "vals" (do (println @*settings*) (println @*updates*) (println @*board*))
+      "vals" (do (println @settings) (println @updates) (println @board))
       "exit" (System/exit 0)
       (println "Unknown Command"))))
 
