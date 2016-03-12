c554efe40704f96cc45407af979ca7e3f727f710
Caleb Phillips <calebrphillips@gmail.com>
2016-03-12 16:23:22 -0500

    Add board parser to store it as a list of vectors

--- bot.clj
+++ bot.clj
@@ -1,23 +1,23 @@
 (ns bot
   (:require [clojure.string :as str]))
 
-(def settings (atom {}))
+(def *settings* (atom {}))
 
-(def updates (atom {}))
+(def *updates* (atom {}))
 
-(def board (atom []))
+(def *board* (atom []))
 
 (defn update-board [board-string] 
-  (println "updating board"))
-
+  (let [vs (vec (str/split board-string #";"))] 
+    (reset! *board* (map #(str/split % #",") vs))))
 
 (defn save-setting [[k v]] 
-  (swap! settings assoc k (read-string v)))
+  (swap! *settings* assoc k (read-string v)))
 
 (defn game-update [[about k v]] 
   (if (= k "field")
     (update-board v)
-    (swap! updates assoc-in [about k] (read-string v))))
+    (swap! *updates* assoc-in [about k] (read-string v))))
 
 ;returns a random action
 (defn get-action [[action t]] 
@@ -29,7 +29,7 @@
       "settings" (save-setting args)
       "update" (game-update args)
       "action" (println (get-action args))
-      "vals" (do (println @settings) (println @updates) (println (@board)))
+      "vals" (do (println @*settings*) (println @*updates*) (println @*board*))
       "exit" (System/exit 0)
       (println "Unknown Command"))))
 
