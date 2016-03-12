fb49a2d47cd650f73c1736f990e19d28eae6e287
Caleb Phillips <calebrphillips@gmail.com>
2016-03-12 15:06:38 -0500

    Use atoms on configs, further implement functions

--- bot.clj
+++ bot.clj
@@ -1,25 +1,37 @@
 (ns bot
   (:require [clojure.string :as str]))
 
-(def settings {})
+(def settings (atom {}))
+
+(def updates (atom {}))
+
+(def board (atom []))
+
+(defn update-board [board-string] 
+  (println "updating board"))
+
 
 (defn save-setting [[k v]] 
-  (assoc settings k v))
+  (swap! settings assoc k (read-string v)))
 
-(defn save-update [[player k v]] 
-  (println k))
+(defn game-update [[about k v]] 
+  (if (= k "field")
+    (update-board v)
+    (swap! updates assoc-in [about k] (read-string v))))
 
-(defn print-action [[action t]] 
-  (println action))
+;returns a random action
+(defn get-action [[action t]] 
+    (str "place-disc " (rand 8)))
 
 (defn parse-input [raw-input] 
   (let [[state & args] (str/split raw-input #"\s")]
     (case state
       "settings" (save-setting args)
-      "update" (save-update args)
-      "action" (print-action args)
-      (throw (Exception. "Invalid input given to bot."))
-      )))
+      "update" (game-update args)
+      "action" (println (get-action args))
+      "vals" (do (println @settings) (println @updates) (println (@board)))
+      "exit" (System/exit 0)
+      (println "Unknown Command"))))
 
 (defn take-input [] 
   (do 
