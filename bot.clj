964fd57b388593a23e9a96990d7585b234267c76
Caleb Phillips <calebrphillips@gmail.com>
2016-03-11 22:15:12 -0500

    Initial commit of bot with input cycle

---
+++ bot.clj
@@ -0,0 +1,29 @@
+(ns bot
+  (:require [clojure.string :as str]))
+
+(defn save-setting [[k v]] 
+  (println k))
+
+(defn save-update [[player k v]] 
+  (println k))
+
+(defn print-action [[action t]] 
+  (println action))
+
+(defn parse-input [raw-input] 
+  (let [[state & args] (str/split raw-input #"\s")]
+    (case state
+      "settings" (save-setting args)
+      "update" (save-update args)
+      "action" (print-action args)
+      (throw (Exception. "Invalid input given to bot."))
+      )))
+
+(defn take-input [] 
+  (do 
+    (parse-input (read-line))
+    (recur)))
+
+(defn -main [] (take-input))
+
+(-main)
